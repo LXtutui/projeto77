7abe8153bec127e8bf7a225d0da3436d9db1841d
LXtutui <113383859+LXtutui@users.noreply.github.com>
2022-12-12 16:29:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("meu primeiro projeto no consoleconsole");
+var primeiro_nome = "Arthur ";
+var ultimo_nome = "Camila";
+ console.log(primeiro_nome);
+ console.log(ultimo_nome);
+var nome_completo = primeiro_nome.concat(ultimo_nome);
+console.log(nome_completo);
+
+var a = 2;
+var b = 16;
+
+var adicao = a + b;
+console.log("a adição de 2 + 16 é: " + adicao);
+var subtracao = a - b;
+console.log("a subitração de 2 - 16 é: " + subtracao);
+var mutiplicacao = a * b;
+console.log("a multiplicação de 2 X 16 é: " + mutiplicacao);
+var divisao = a / b;
+console.log("a divisão de 2 / 16 é: " + divisao);
+
+var raiz = Math.sqrt(b);
+console.log("a raiz quadrada de 12 é" + raiz);
+
+var expoente = Math.pow(a, b);
+console.log("dez elevado a dois é: " + expoente);
